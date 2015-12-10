4f30213219c3a537f005c2fc7b1eae54685765e6
Ian <championfoxhound@gmail.com>
2015-12-10 17:30:57 -0600

    fixed register page

--- frontend/javascripts/register.js
+++ frontend/javascripts/register.js
@@ -7,9 +7,9 @@ app.controller('RegisterCtrl', function ($scope, $http) {
   $scope.passwordConfirm = "";
   $scope.error = false;
   $scope.errorMessage = "";
-  
+
   $scope.submit = function(){
-  	console.log("Entered submit function");
+    console.log("Entered submit function");
 
     if($scope.username === ""){
       $scope.error = true;
@@ -35,9 +35,20 @@ app.controller('RegisterCtrl', function ($scope, $http) {
     console.log("created login");
 
     $http.post('/register', login).then(function successCallback(response){
-   		console.log("success"); 	
+      console.log("success");     
+        if(response.data.success === true){
+          sessionStorage.setItem('username', $scope.username);
+          window.location.href = "/groups.html";
+        }
+        else {
+          $scope.error = true;        
+          $scope.errorMessage = "Username is taken";
+        }
+
     }, function errorCallback(response){
-    	console.log("error");
+      console.log("error");
+        $scope.error = true;        
+        $scope.errorMessage = "Username is taken";
     });
   }
 });
